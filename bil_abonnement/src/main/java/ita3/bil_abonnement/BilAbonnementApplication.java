9fd8be6231e8edaaa7bc131be9c7b473ebf18d9a
Amalie956 <amal956e@stud.kea.dk>
2023-11-30 22:07:56 +0100

    Push Spring Boot project på Github

---
+++ bil_abonnement/src/main/java/ita3/bil_abonnement/BilAbonnementApplication.java
@@ -0,0 +1,13 @@
+package ita3.bil_abonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilAbonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilAbonnementApplication.class, args);
+    }
+
+}
---
+++ bil_abonnement/src/test/java/ita3/bil_abonnement/BilAbonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package ita3.bil_abonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
