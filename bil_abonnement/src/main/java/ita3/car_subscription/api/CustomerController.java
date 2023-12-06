8f55b54b88b180337e328e7eda73a46c5b1769e8
Amalie956 <amal956e@stud.kea.dk>
2023-12-06 11:43:41 +0100

    Tilføjet @CrossOrigin("*")

--- bil_abonnement/src/main/java/ita3/car_subscription/api/CustomerController.java
+++ bil_abonnement/src/main/java/ita3/car_subscription/api/CustomerController.java
@@ -1,9 +1,11 @@
 package ita3.car_subscription.api;
 
 import ita3.car_subscription.repository.ICustomerRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class CustomerController {
     private ICustomerRepository customerRepository;
 
